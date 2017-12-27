38b6639e4c7e1dccc9a1a6f12a4151e45838dc12
roballred <roballred@hotmail.com>
2017-12-27 11:20:06 -0800

    Initial commit of module

---
+++ Services/LimitLoginService.cs
@@ -0,0 +1,21 @@
+﻿using Orchard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Allred.LimitLogin.Services
+{
+
+    public interface ILimitLoginService : IDependency
+    {
+        bool IsAccountLocked(string userName);
+    }
+    public class LimitLoginService : ILimitLoginService
+    {
+        public bool IsAccountLocked(string userName)
+        {
+            return true;
+        }
+    }
+}
